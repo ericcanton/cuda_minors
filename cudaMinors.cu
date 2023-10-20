e30549829399e315c4dee876f9650c107ccb0744
Eric Canton <eric.d.canton@gmail.com>
2023-10-20 07:20:18 -0500

    start 2x2 id test. start adding test dir.

--- cudaMinors.cu
+++ cudaMinors.cu
@@ -1,4 +1,3 @@
-#include <concepts>
 #include <iostream>
 #include <thrust/device_vector.h>
 #include <thrust/host_vector.h>
@@ -6,73 +5,23 @@
 
 #include "device_minors.cuh"
 
-template <typename T>
-concept Sized = requires(T t) {
-    {
-        t.size()
-    } -> std::convertible_to<size_t>;
-};
-
-void print_vector(Sized auto vec)
-{
-    for (int i = 0; i < vec.size(); i++)
-    {
-        std::cout << vec[i] << " ";
-    }
-    std::cout << std::endl;
-}
-
 int main()
 {
-    int N = 5;
-    int numBlocks = 1;
-    while (numBlocks < N)
-    {
-        numBlocks = numBlocks << 1;
-    }
-    int numMinors = (5 * 4) / 2;
+    int m = 2, n = 2;
+    int A[m * n] = {0};
+    int minors[1] = {1};
+    A[0] = 1;
+    A[3] = 1;
+    std::cout << "A: " << std::endl;
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            std::cout << A[idx(i, j, 2)] << " ";
+    std::cout << "===" << std::endl;
 
-    // initialize 2 x N matrix with 0..<2N-1
-    thrust::device_vector<int> mat(2 * N);
-    thrust::sequence(mat.begin(), mat.end());
+    // allocate device memory, copy A to device
 
-    thrust::device_vector<int> minors(numMinors);
-    thrust::host_vector<int> d_minors_result(numMinors);
+    minors2xN_device(5, 5, minors, A, n);
+    std::cout << "Got this minor from device: " << minors[0] << std::endl;
 
-    // do the same on the host
-    thrust::host_vector<int> h_mat(2 * N);
-    thrust::sequence(h_mat.begin(), h_mat.end());
-
-    thrust::host_vector<int> h_minors(numMinors);
-
-    // call the kernel
-    minors2xN_device(numBlocks, numBlocks - 1, minors, mat, N);
-
-    // copy the result back to the host
-    thrust::copy(minors.begin(), minors.end(), d_minors_result.begin());
-
-    // do the same on the host
-    // minors2xN_cpu(h_minors, h_mat, N);
-
-    // print the vectors of minors
-    std::cout << "Device: ";
-
-    // std::cout << "CPU: ";
-    // for (int i = 0; i < numMinors; i++)
-    // {
-    //     std::cout << h_minors[i] << " ";
-    // }
-    // std::cout << std::endl;
-
-    // // check the result
-    // for (int i = 0; i < numMinors; i++)
-    // {
-    //     if (h_minors[i] != d_minors_result[i])
-    //     {
-    //         std::cout << "Error at index " << i << std::endl;
-    //         std::cout << "Host: " << h_minors[i] << std::endl;
-    //         std::cout << "Device: " << d_minors_result[i] << std::endl;
-    //         return 1;
-    //     }
-    // }
+    thrust::device_vector<int> minors_expected(1, 1.0);
 }
--- src/lib/device_minors.cu
+++ src/lib/device_minors.cu
@@ -1,13 +1,20 @@
 
 #include "device_minors.cuh"
 #include <iostream>
+#include <stdio.h>
 
 __global__ void minors2xN_kernel(int *minors_out, int *matrix, size_t N)
 {
     auto col1 = blockIdx.x * blockDim.x + threadIdx.x;
     auto col2 = blockIdx.y * blockDim.y + threadIdx.y;
+    printf("\n===\ncol1: %d col2: %d", col1, col2);
     if (col2 > col1 || col1 >= N || col2 >= N)
+    {
+        printf("@@exceeds matrix size\n");
         return;
+    }
+    printf("\n");
+    printf("to index: %d\n", idx(col1, col2, N));
     minors_out[idx(col1, col2, N)] = minor2x2_2xN(matrix, col1, col2, N);
 }
 
@@ -18,6 +25,7 @@ void minors2xN_cpu(int *minors_out, int *matrix, size_t N)
         for (size_t col2 = col1 + 1; col2 < N; col2++)
         {
             size_t index = idx(col1, col2, N);
+            std::cout << "\n===\ncol1: " << col1 << " col2: " << col2 << "\n";
             minors_out[index] = minor2x2_2xN(matrix, col1, col2, N);
             std::cout << minors_out[index] << " ";
         }
